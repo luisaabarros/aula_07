27a09da56efd04c99d033464d18ed531734ccc2c
luisaabarros <luisaabarros1707@gmail.com>
2025-12-04 21:56:23 -0300

    Adiciona exercícios de lista e dicionário.

--- aula07/exemplo03.py
+++ aula07/exemplo03.py
@@ -1,14 +1,14 @@
-lista_numeros = []
+# lista_numeros = []
 
-for n in range(5):
-    num = float(input('Informe o numero: '))
-    lista_numeros.append(num)
+# for n in range(5):
+#     num = float(input('Informe o numero: '))
+#     lista_numeros.append(num)
 
 
-print(lista_numeros[0])
+# print(lista_numeros[0])
 
-lista_numeros[0] = 22 # Altera o valor do índice 0
-lista_numeros.pop(-2)  # Deleta o último
-lista_numeros.remove(30) # Deleta pelo valor
-del lista_numeros[0]
-print(lista_numeros)
+# lista_numeros[0] = 22 # Altera o valor do índice 0
+# lista_numeros.pop(-2)  # Deleta o último
+# lista_numeros.remove(30) # Deleta pelo valor
+# del lista_numeros[0]
+# print(lista_numeros)
